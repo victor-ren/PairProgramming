988b764c0f3d4fe57d1b4e0579cf2afdee3d02c8
harvey940906 <zhangjunfly@gmail.com>
2014-03-02 21:24:01 -0600

    Update RainGame.java

--- RainGame.java
+++ RainGame.java
@@ -1,6 +1,6 @@
 //UIUC CS125 SPRING 2014 MP. File: RainGame.java, CS125 Project: PairProgramming, Version: 2014-02-24T21:11:32-0600.309819000
 /**
- * @author replace-this-with-your-netids-on-this-line-here-with-a-comma-between-them
+ * @author jzhng125,weiren2
  */
 public class RainGame {
 
@@ -20,16 +20,14 @@ public class RainGame {
 			if (text.length() == 0) {
 				x = (int) (Math.random()*500);
 				y = (int) (Math.random()*500);
-				dx = 2;
-				dy = 2;
+				dx = 2*level;
+				dy = 2*level;
 				text = "" + (int) (Math.random() * 999);
-				long elapsed = System.currentTimeMillis() - startTime;
-				startTime = System.currentTimeMillis();
-				score += 7000 / elapsed;
+
 			}
 			Zen.setColor(0, 0, 0);
 			Zen.fillRect(0, 0, Zen.getZenWidth(), Zen.getZenHeight());
-
+			
 			Zen.setColor(0, 255, 0);
 			Zen.drawText(text, x, y);
 			
@@ -43,21 +41,29 @@ public class RainGame {
 			x += dx;
 			y += dy;
 			
+			if(x>(Zen.getZenWidth()+30) || y>(Zen.getZenHeight()+30)){
+				x = (int) (Math.random()*500);
+				y = (int) (Math.random()*500);
+			}
 			// Find out what keys the user has been pressing.
 			String user = Zen.getEditText();
 			// Reset the keyboard input to an empty string
 			// So next iteration we will only get the most recently pressed keys.
 			Zen.setEditText("");
-			
+			int singlescore = 0;
 			for(int i=0;i < user.length();i++) {
 				char c = user.charAt(i);
-				if(c == text.charAt(0))
+				if(c == text.charAt(0)){
 					text = text.substring(1,text.length()); // all except first character
+				singlescore++;}
 			}
-			
+			score += singlescore;
 			Zen.sleep(90);// sleep for 90 milliseconds
 
 		}
+		long elapsed = System.currentTimeMillis() - startTime;
+		startTime = System.currentTimeMillis();
+		score += 7000 / elapsed;
 	}
 
 }
