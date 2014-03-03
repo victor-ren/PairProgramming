2fa3633119f65d4ef74c959b1bfb1440a194ce3c
harvey940906 <zhangjunfly@gmail.com>
2014-03-02 23:46:46 -0600

    Update RainGame.java

--- RainGame.java
+++ RainGame.java
@@ -1,6 +1,6 @@
 //UIUC CS125 SPRING 2014 MP. File: RainGame.java, CS125 Project: PairProgramming, Version: 2014-02-24T21:11:30-0600.295816000
 /**
- * @author jzhng125
+ * @author jzhng125,weiren2
  */
 public class RainGame {
 
@@ -12,19 +12,28 @@ public class RainGame {
 	
 		int x=0, y=0, dx=0, dy=0, score = 0, level = 1, singlescore = 0;
 		String text = "";
-		long startTime =System.currentTimeMillis();
+		boolean begin = false, gameover = false;
 		
 		Zen.setFont("Helvetica-32");
+		
 		while (Zen.isRunning()) {
-
-			if (text.length() == 0) {
-				x = (int) (Math.random()*200);
-				y = (int) (Math.random()*200);
-				dx = 2*level;
-				dy = 2*level;
-				text = "" + (int) (Math.random() * 999);
+			while (!begin){
+				Zen.setColor(0, 255, 0);
+				Zen.drawText("Which level would you like begin?(1-9)",100,260);
+				Zen.flipBuffer();
+			String chooselevel = Zen.getEditText();
+			for(int i=1;i<=9;i++){
+				if(chooselevel.equals(i+"")){
+					level = i;
+					score = i*10-9;
+					begin = true;
+				}
 			}
-			Zen.setColor(255, 0, 255);
+			}
+			while(begin){
+			
+            if(!gameover){
+			Zen.setColor(0, 0, 0);
 			Zen.fillRect(0, 0, Zen.getZenWidth(), Zen.getZenHeight());
 
 			Zen.setColor(0, 255, 0);
@@ -33,6 +42,17 @@ public class RainGame {
 			Zen.drawText("Level: "+level,10,30);
 			Zen.drawText("Score: "+score,10,60);
 			Zen.flipBuffer();
+			
+			
+
+			if (text.length() == 0) {
+				x = (int) (Math.random()*200);
+				y = (int) (Math.random()*200);
+				dx = 2*level;
+				dy = 2*level;
+				text = "" + (int) (Math.random() * 999);
+			}
+			
 			
 			x += dx;
 			y += dy;
@@ -65,8 +85,23 @@ public class RainGame {
 			else if((score%10+singlescore)<0 && singlescore < 0 ){
 				level--;}
 			Zen.sleep(50);// sleep for 90 milliseconds
-
+			
+			if(score<0){
+				gameover = true;
+			}
+            }
+			else{
+            
+			Zen.setColor(0, 0, 0);
+			Zen.fillRect(0, 0, Zen.getZenWidth(), Zen.getZenHeight());
+			
+			Zen.setColor(255, 0, 0);
+			Zen.drawText("Game Over",160,150);
+			Zen.flipBuffer();
+            }
+			}
 		}
+
 	}
 
 }
