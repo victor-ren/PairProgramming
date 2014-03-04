631c4b11b95502dcee803cadc4572394c0e57b1f
Bite Me! <victor-ren@outlook.com>
2014-03-03 21:32:10 -0600

    Update RainGame.java

--- RainGame.java
+++ RainGame.java
@@ -2,6 +2,7 @@
 /**
  * @author jzhng125,weiren2
  */
+
 public class RainGame {
 
 	public static void main(String[] args) {
@@ -17,117 +18,166 @@ public class RainGame {
 		Zen.setFont("Helvetica-32");
 		
 		while (Zen.isRunning()) {
+			
 			while (!begin){
 				Zen.setColor(0, 191, 255);
-				Zen.drawText("Which level would you like begin?(1-9)",50,230);
+				Zen.drawText("Which level would you like begin?(1-9)",40,230);
 				Zen.flipBuffer();
-			String chooselevel = Zen.getEditText();
-			for(int i=1;i<=9;i++){
-				if(chooselevel.equals(i+"")){
+				String chooselevel = Zen.getEditText();
+				
+				for(int i=1;i<=9;i++){
+					if(chooselevel.equals(i+"")){
 					level = i;
-					score = i*10-10;
+					score = (i)*5;
 					begin = true;
+					}
 				}
+				Zen.setEditText("");
 			}
-			Zen.setEditText("");
-			}
+			
 			while(begin){
 			
-            if(!gameover){
-			Zen.setColor(0,0,0);
-			Zen.fillRect(0, 0, Zen.getZenWidth(), Zen.getZenHeight());
+				if(!gameover){
+					Zen.setColor(0,0,0);
+					Zen.fillRect(0, 0, Zen.getZenWidth(), Zen.getZenHeight());
 
-			Zen.setColor(64, 224, 208);
-			Zen.drawText(text, x, y);
+					
+					Zen.setColor(64, 224, 208);
+					Zen.drawText(text, x, y);
+					
 			
-			Zen.drawText("Level: "+level,10,30);
-			Zen.drawText("Score: "+score,10,60);
-			Zen.flipBuffer();
+					Zen.drawText("Level: "+level,10,30);
+					Zen.drawText("Score: "+score,10,60);
+					Zen.flipBuffer();
 			
 			
 
-			if (text.length() == 0) {
-				x = (int) (Math.random()*200);
-				y = (int) (Math.random()*200);
-				dx = 1;
-				dy = 1;
-				text = "" + (int) (Math.random() * 999);
-			}
+					if (text.length() == 0) {
+						x = (int) (Math.random()*280)+20;
+						y = (int) (Math.random()*190)+60;
+						
+						if (Math.random()>0.5 && level>2) 
+							dx = level;
+						else if (Math.random()>0.5 && level>2){
+							x = (int) (Math.random()*300)+300;
+							//y = (int) (Math.random()*250)+250;
+							dx = -level;
+						}else
+							dx=0;
+						
+						if (Math.random()>0.5 && level>2) 
+							dy = level;
+						else if (Math.random()>0.5 && level>2){
+							//x = (int) (Math.random()*280)+20;
+							y = (int) (Math.random()*250)+250;
+							dy = -level;
+						}
+						else
+							dy=0;
+						
+						if (dx==0 && dy==0){
+							dx=level;
+							dy=level;
+						}
+						text = "" + (int) (Math.random() * 999);
+					}
 			
 			
-			x += dx;
-			y += dy;
+					if (level>0 && level<10) {
+						x += dx;
+						y += dy;
+					}else if(level>9 && level%((int)(Math.random()*5)+1)==0){
+						if (dx!=0) x += dx+(int)(Math.random()*5);
+						else x +=dx;
+						
+						if (dy!=0) y += dy+(int)(Math.random()*5);
+						else y +=dy;
+					}else{
+						x += dx;
+						y += dy;
+					}
 			
 			// Find out what keys the user has been pressing.
-			String user = Zen.getEditText();
+					String user = Zen.getEditText();
 			// Reset the keyboard input to an empty string
 			// So next iteration we will only get the most recently pressed keys.
-			Zen.setEditText("");
+					Zen.setEditText("");
 			
-			singlescore = 0;
-			for(int i=0;i < user.length();i++) {
-				char c = user.charAt(i);
-				if(!user.equals("") && c == text.charAt(0)){
-					text = text.substring(1,text.length()); // all except first character
-					singlescore++;
-				}
-				else if(!user.equals("") && c != text.charAt(0)){
-					text = text.substring(1,text.length()); // all except first character
-					singlescore--;
-				}
-			}
-			if(x>(Zen.getZenWidth()+9*text.length()) || y>(Zen.getZenHeight()+9*text.length())){
-				x = (int) (Math.random()*200);
-				y = (int) (Math.random()*200);
-			    singlescore -= text.length();}	
+					singlescore = 0;
+					for(int i=0;i < user.length();i++) {
+						char c = user.charAt(i);
+						if(!text.equals("") && !user.equals("")&&c == text.charAt(0)){
+							text = text.substring(1,text.length()); // all except first character
+							singlescore++;
+						}
+						else if(!text.equals("") && !user.equals("") && c != text.charAt(0)){
+							text = text.substring(1,text.length()); // all except first character
+							singlescore--;
+						}
+					}
+					
+					if(x>(Zen.getZenWidth()+9*text.length()) || y>(Zen.getZenHeight()+9*text.length())){
+						x = (int) (Math.random()*200)+60;
+						y = (int) (Math.random()*200)+60;
+						singlescore -= text.length();
+					}else if (x<(-9*text.length()) || y<(-9*text.length())){
+						x = (int) (Math.random()*200)+300;
+						y = (int) (Math.random()*200)+200;
+						singlescore -= text.length();
+					}
 			
-			if(score%10 == 9 && singlescore > 0){
-				level++;
-				prelevel = level-1;}
-			else if((score%10+singlescore)<0 && singlescore < 0 ){
-				level--;
-				prelevel = level+1;}
-			Zen.sleep(50);// sleep for 90 milliseconds
-			score += singlescore;
+					if(score%5 == 4 && singlescore > 0){
+						level++;
+						prelevel = level-1;
+					}
+					else if((score%5+singlescore)<0 && singlescore < 0 ){
+						level--;
+						prelevel = level+1;
+					}
+					
+					Zen.sleep(50);// sleep for 90 milliseconds
+					score += singlescore;
 			
-			if(score<0 || level <=0){
-				gameover = true;
-			}
-			if(level%10 == 0 && level != 0 && prelevel < level){
-				congratulate = true;
-				prelevel = level;
-			}
-			while(congratulate && cx<(Zen.getZenWidth()+50)){
-			Zen.setColor(64, 224, 208);
-			Zen.drawText("Level: "+level,10,30);
-			Zen.drawText("Score: "+score,10,60);
-			Zen.setFont("Helvetica-64");
-			Zen.drawText("GG, Keep Up!", cx, cy);
-			Zen.setFont("Helvetica-32");
-			cx+=dcx;
-			Zen.flipBuffer();
-			Zen.sleep(40);
-			}
+					if(score<0 || level <=0){
+						gameover = true;
+					}
+					
+					if(level%5 == 0 && level != 0 && prelevel < level && text.equals("")){
+						congratulate = true;
+						prelevel = level;
+					}
+					
+					while(congratulate && cx<(Zen.getZenWidth()+50)){
+						Zen.setColor(64, 224, 208);
+						Zen.drawText("Level: "+level,10,30);
+						Zen.drawText("Score: "+score,10,60);
+						Zen.setFont("Helvetica-64");
+						Zen.drawText("GG, Keep Up!", cx, cy);
+						Zen.setFont("Helvetica-32");
+						cx+=dcx;	
+						Zen.flipBuffer();
+						Zen.sleep(30);
+					}	
             
-			if(cx>=(Zen.getZenWidth()+50)){
-				cx=-200;
-				congratulate = false;
-			}
-			}
+					if(cx>=(Zen.getZenWidth()+50)){
+						cx=-200;
+						congratulate = false;
+					}
+				}
  
 			else{
             
-			Zen.setColor(0, 0, 0);
-			Zen.fillRect(0, 0, Zen.getZenWidth(), Zen.getZenHeight());
+				Zen.setColor(0, 0, 0);
+				Zen.fillRect(0, 0, Zen.getZenWidth(), Zen.getZenHeight());
 			
-			Zen.setColor(255, 0, 0);
-			Zen.drawText("Game Over",235,240);
-			Zen.flipBuffer();
+				Zen.setColor(255, 0, 0);
+				Zen.setFont("Helvetica-80");
+				Zen.drawText("Game Over",110,120);
+				Zen.flipBuffer();
             }
-			}
 		}
-
 	}
 
-	}
+}
 
+}
